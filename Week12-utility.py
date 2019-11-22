ab47371a566a3214ff9b727e250ff966ce5302b6
school <school@csm-wl-dhcp-235-40.mines.edu>
2019-11-21 22:47:21 -0700

    added 2 new functions

--- Week12-utility.py
+++ Week12-utility.py
@@ -14,3 +14,24 @@ def LoadFile(filename):
     return output
 
 
+def UpdateString (string, char, index):
+    output = ""
+    for a in range(0,len(string)):
+        if a == index:
+            output = output + char
+        else:
+            output = output + string[a]
+    return output
+
+
+def FindWordCount(list, word):
+    count = 0
+    for words in list:
+        if words == word:
+            count = count + 1
+    return count
+
+
+
+a = LoadFile("test.txt")
+PrintOutput(str(FindWordCount(a, 'adam')))
